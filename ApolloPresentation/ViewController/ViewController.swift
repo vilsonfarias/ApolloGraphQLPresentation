a79c5f6e0c8e3071230fb2cdb4a61c1ed8e4c7d1
Vilson Farias <vilson.farias@walmart.com>
2021-04-11 10:07:48 -0300

    Infinite loading

--- ApolloPresentation/ViewController/ViewController.swift
+++ ApolloPresentation/ViewController/ViewController.swift
@@ -12,12 +12,20 @@ class LaunchViewController: UIViewController {
 
     @IBOutlet weak var launchesTableView: UITableView!
 
-    var launches = [LaunchListQuery.Data.Launch.Launch]()
+    private var launches = [LaunchListQuery.Data.Launch.Launch]()
+    private var lastConnection: LaunchListQuery.Data.Launch?
+    private var activeRequest: Cancellable?
+
+    enum ListSection: Int, CaseIterable {
+      case launches
+      case loading
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         launchesTableView.dataSource = self
-        loadLaunches()
+        launchesTableView.delegate = self
+        loadMoreLaunchesIfTheyExist()
     }
 
     func showErrorAlert(title: String, message: String) {
@@ -27,25 +35,49 @@ class LaunchViewController: UIViewController {
 
 //MARK:- GraphQL
 
-extension LaunchViewController {
-    private func loadLaunches() {
-        Network.shared.apollo
-            .fetch(query: LaunchListQuery()) { [weak self] result in
-                guard let self = self else { return }
-                defer { self.launchesTableView.reloadData() }
+private extension LaunchViewController {
+    func loadMoreLaunches(from cursor: String?) {
+        activeRequest = Network.shared.apollo.fetch(query: LaunchListQuery(cursor: cursor)) { [weak self] result in
+            guard let self = self else { return }
+            self.activeRequest = nil
+            defer { self.launchesTableView.reloadData() }
 
-                switch result {
-                case .success(let graphQLResult):
-                    self.handleResponse(data: graphQLResult.data,
-                                        errors: graphQLResult.errors)
-                case .failure(let error):
-                    self.showErrorAlert(title: "Network Error",
-                                        message: error.localizedDescription)
+            switch result {
+            case .success(let graphQLResult):
+                if let launchConnection = graphQLResult.data?.launches {
+                    self.lastConnection = launchConnection
+                    self.launches.append(contentsOf: launchConnection.launches.compactMap { $0 })
                 }
+
+                if let errors = graphQLResult.errors {
+                    let message = errors
+                        .map { $0.localizedDescription }
+                        .joined(separator: "\n")
+
+                    self.showErrorAlert(title: "GraphQL Error(s)", message: message)
+                }
+            case .failure(let error):
+                self.showErrorAlert(title: "Network Error", message: error.localizedDescription)
             }
+        }
     }
 
-    private func handleResponse(data: LaunchListQuery.Data?, errors: [GraphQLError]?) {
+    func loadMoreLaunchesIfTheyExist() {
+      guard let connection = self.lastConnection else {
+        // We don't have stored launch details, load from scratch
+        self.loadMoreLaunches(from: nil)
+        return
+      }
+
+      guard connection.hasMore else {
+        // No more launches to fetch
+        return
+      }
+
+      self.loadMoreLaunches(from: connection.cursor)
+    }
+
+    func handleResponse(data: LaunchListQuery.Data?, errors: [GraphQLError]?) {
         if let launchConnection = data?.launches {
           self.launches.append(contentsOf: launchConnection.launches.compactMap { $0 })
         }
@@ -60,12 +92,37 @@ extension LaunchViewController {
     }
 }
 
+//MARK:- TableView Delegate
+
+extension LaunchViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if indexPath.section == tableView.numberOfSections - 1 &&
+            indexPath.row == tableView.numberOfRows(inSection: indexPath.section) - 1 {
+            loadMoreLaunchesIfTheyExist()
+        }
+    }
+}
+
 //MARK:- TableView Data Source
 
 extension LaunchViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         print("numberOfRowsInSection: \(launches.count)")
-        return launches.count
+        guard let listSection = ListSection(rawValue: section) else {
+          assertionFailure("Invalid section")
+          return 0
+        }
+
+        switch listSection {
+        case .launches:
+            return self.launches.count
+        case .loading:
+            if self.lastConnection?.hasMore == false {
+                return 0
+            } else {
+                return 1
+            }
+        }
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -75,10 +132,26 @@ extension LaunchViewController: UITableViewDataSource {
             return UITableViewCell()
         }
 
-        let launch = self.launches[indexPath.row]
-        cell.update(launchId: launch.id,
-                    launchSite: launch.site,
-                    spaceShip: launch.rocket?.name)
+        guard let listSection = ListSection(rawValue: indexPath.section) else {
+            assertionFailure("Invalid section")
+            return cell
+        }
+
+        switch listSection {
+        case .loading:
+            if self.activeRequest == nil {
+                cell.textLabel?.text = "Tap to load more"
+            } else {
+                cell.update(launchId: "Loading",
+                            launchSite: "Loading",
+                            spaceShip: "Loading")
+            }
+        case .launches:
+            let launch = self.launches[indexPath.row]
+            cell.update(launchId: launch.id,
+                        launchSite: launch.site,
+                        spaceShip: launch.rocket?.name)
+        }
 
         return cell
     }
